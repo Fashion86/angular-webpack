a5c05bd84987caf8aeb9891b95d588e635d6e5b2
hong86 <msjkgoodluck@gmail.com>
2019-03-23 18:49:42 +0800

    update agular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PppComponent } from './ppp/ppp.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent,
-    PppComponent
+    AppComponent
   ],
   bootstrap: [ AppComponent ]
 })
